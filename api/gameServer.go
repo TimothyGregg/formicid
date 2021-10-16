0dfd153a551631859fe4bcb70fc5fd011a5a4bfc
Tim <Timothy.MGregg@gmail.com>
2021-10-16 18:25:53 -0400

    Working on middleware

---
+++ api/gameServer.go
@@ -0,0 +1,88 @@
+package api
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/TimothyGregg/Antmound/game"
+	"github.com/TimothyGregg/Antmound/game/tools"
+	"github.com/gorilla/mux"
+)
+
+type GameServer struct {
+	http.Server
+	Games []*game.Game
+	UID_Generator *tools.UID_Generator
+}
+
+func New_GameServer() (*GameServer) {
+	gs := &GameServer{}
+	gs.UID_Generator = tools.New_UID_Generator()
+
+	// build router
+	router := mux.NewRouter().StrictSlash(true)	
+	router.HandleFunc("/", gs.apiPost)
+	router.HandleFunc("/games", gs.returnGames).Methods("GET")
+    router.HandleFunc("/games/{id}", gs.returnGameByID).Methods("GET")
+	gs.Handler = router
+
+	// get the appropriate port to serve on
+	port := os.Getenv("PORT")
+	gs.Addr = ":" + port
+
+	return gs
+}
+
+func (gs *GameServer) Start() {
+	log.Fatal(gs.ListenAndServe())
+}
+
+func (gs *GameServer) New_Game(size_x, size_y, tries int) {
+	gs.Games = append(gs.Games, game.New_Game(gs.UID_Generator.Next(), size_x, size_y, tries))
+}
+
+func (gs *GameServer) apiPost(w http.ResponseWriter, r *http.Request) {
+	pr := &PostRequest{r}
+	headerContentType := pr.Header.Get("Content-Type") //https://golangbyexample.com/validate-range-http-body-golang/
+	fmt.Println("Endpoint Hit: post")
+	if headerContentType != "application/json" {
+		errorResponse(w, "Content Type is not application/json", http.StatusUnsupportedMediaType)
+		return
+	}
+}
+
+func (gs *GameServer) returnGames(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: returnGames")
+	if len(gs.Games) == 0 {
+		errorResponse(w, "No Games", http.StatusNotFound)
+	} else {
+		json.NewEncoder(w).Encode(gs.Games)
+	}
+}
+
+func (gs *GameServer) returnGameByID(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key, _ := strconv.ParseInt(vars["id"], 10, 64)
+	if int(key) < len(gs.Games) {
+		json.NewEncoder(w).Encode(gs.Games[int(key)])
+	} else {
+		errorResponse(w, "Game not found", http.StatusNotFound)
+	}
+}
+
+func errorResponse(w http.ResponseWriter, message string, httpStatusCode int) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(httpStatusCode)
+	resp := make(map[string]string)
+	resp["message"] = message
+	jsonResp, _ := json.Marshal(resp)
+	w.Write(jsonResp)
+}
+
+func ok(w http.ResponseWriter) {
+	errorResponse(w, "Success", http.StatusOK)
+}
--- main.go
+++ main.go
@@ -1,11 +1,30 @@
 package main
 
 import (
+	"fmt"
+	"sync"
+
 	"github.com/TimothyGregg/Antmound/api"
+	"github.com/TimothyGregg/Antmound/frontend"
 )
 
 func main() {
-	s := api.New_Server()
-	s.New_Game(100, 100, 10)
-	s.HandleRequests()
+	wg := new(sync.WaitGroup)
+	wg.Add(2)
+
+	gs := api.New_GameServer()
+	fs := frontend.New_FrontendServer()
+
+	go func() {
+		gs.Start()
+		wg.Done()
+	}()
+	fmt.Println("1")
+	go func() {
+		fs.Start()
+		wg.Done()
+	}()
+	fmt.Println("2")
+
+	wg.Wait()
 }
